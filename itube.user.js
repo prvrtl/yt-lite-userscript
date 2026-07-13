3015915c9ebdd5f49b30caa68b7e3593d1a528b9
Roman Perevertailo <29702518+prvrtl@users.noreply.github.com>
2026-07-13 08:56:04 +0200

    inline the script icon as a data URI so it renders without a network fetch

--- itube.user.js
+++ itube.user.js
@@ -11,7 +11,7 @@
 // @supportURL   https://github.com/prvrtl/yt-lite-userscript/issues
 // @updateURL    https://raw.githubusercontent.com/prvrtl/yt-lite-userscript/main/itube.user.js
 // @downloadURL  https://raw.githubusercontent.com/prvrtl/yt-lite-userscript/main/itube.user.js
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @icon         data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2064%2064%22%3E%3Crect%20width%3D%2264%22%20height%3D%2264%22%20rx%3D%2214%22%20fill%3D%22%230a84ff%22%2F%3E%3Cpath%20d%3D%22M25%2020.5v23l19-11.5z%22%20fill%3D%22%23fff%22%2F%3E%3C%2Fsvg%3E
 // @match        https://www.youtube.com/*
 // @exclude      https://www.youtube.com/embed/*
 // @exclude      https://www.youtube.com/live_chat*
